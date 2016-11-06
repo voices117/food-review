af771dcd721dbc94d0b232a31ac57f6a04adde11
MauroFab <maurotoscano2@gmail.com>
2016-11-06 17:48:26 -0300

    Emprolijado el nnet, y puesto para que use TF IDF. Puesto para tomar un sample chiquito al principio y variable, para hacer pruebas rapidas

--- nnet.r
+++ nnet.r
@@ -1,42 +1,38 @@
-#No correr con menos de 12 gib de memoria. Para probar con menos bajar la cantidad de elementos
-
 library(NLP)
 library(tm)
 library(nnet)
+library(dplyr)
+#Trabajo con pocos datos para testear, dummy test
+set.seed(123)
+#Tiro datos ya desde un inicio, esto es para acelerar pruebas. 
+#Se puede poner en 1 o comentar para no tirar
 
-corpus <- Corpus(VectorSource(trainPreProcesado$Text))
-dtm <- DocumentTermMatrix(corpus)
+preProcesado<-sample_frac(trainPreProcesado, 0.5)
+
+corpus <- Corpus(VectorSource(preProcesado$Text))
+#Actualmente usa TF, puede cambiarse por TF-IDF
+dtm <- DocumentTermMatrix(corpus, control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE)))
 #Con 0.99 quedan mas o menos 650 terminos. Con mas empiezo a tener problemas de memoria
-dtmr <- removeSparseTerms(dtm, 0.99)
 rm(corpus)
+dtmr <- removeSparseTerms(dtm, 0.99)
 rm(dtm)
 m <- as.matrix(dtmr)
 #Normalizo
-#Scale no se comporta bien para matrices grandes
-#m <- scale(m, center = FALSE, scale = TRUE)
-#Apply es como map, y sweep "hereda" de apply
-#2 indica que por columnas aplico la funcion maximo.
-maxs <- apply(m,2, max)
-#Con sweep divido cada columna por el maximo
-#Cuidado que a veces no lo toma y hay que mandarlo de nuevo
-m <- sweep(m, 2, maxs, FUN="/")
-rm(maxs)
-#Con 250000 datos me alcanzan mis 12 gib de memoria ram. Con mas no, pero deberia poder variar.
-m <- m[1:250000,]
-trainReducido <- trainPreProcesado[1:250000,]
-nn <- nnet(m[1:250000], (trainPreProcesado$Prediction-1)/4, size = 1)
+  m <- scale(m)
+  
+#Preparo el train, va a ser chico para que alcance la memoria y el tiempo
+set.seed(123)
+filasTrain <- sample(nrow(m), size = floor(nrow(m)*0.5))
+train <- m[filasTrain,]
 
-#Para testear, y liberar algo de la memoria antes. Si el sistema operativo se queda sin nada
-#usar un gc()
-m <- as.matrix(dtmr)
-m <- sweep(m, 2, maxs, FUN="/")
-m<-m[300000:400000,]
-rm(maxs)
-prediction <- predict(nn, m)
-prediction <- (prediction+0.25)*4
-real <- trainPreProcesado$Prediction[300000:400000]
-diferencia <- prediction - realM
-diferenciaCuadrada <- diferencia^2
-sumaDiferenciasCuadradas <- colSums(diferenciaCuadrada)
-varianza <- sumaDiferenciasCuadradas/nrow(diferenciaCuadrada)
-desvio <- sqrt(varianza)
+#Armo el vector Y para ajustar
+trainY <- preProcesado[filasTrain,]$Prediction
+#Nota: La Y no es necesario normalizarla en una red Neuronal. Asi lo lei en varios sitios
+nn <- nnet(train, trainY, size = 1, linout = TRUE)
+
+#Libero la memoria del train, y me quedo con el test
+rm(train)
+test <- m[-filasTrain,]
+#Me quedo con el test
+prediction <- predict(nn, test)
+head(prediction)
